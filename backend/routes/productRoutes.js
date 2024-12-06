457b2197749ec7e18530eb0f67d379defc3a4773
114514Cocoa <yiqli965@gmail.com>
2024-12-05 21:27:29 -0800

    Search Products && Product Carousel

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createdProductReview } from '../controllers/productController.js';
+import { getProducts, getProductById, createProduct, updateProduct, deleteProduct, createdProductReview, getTopProducts } from '../controllers/productController.js';
 import { protect, admin } from '../middleware/authMiddleware.js' 
 
 const router = express.Router();
 
 router.route('/').get(getProducts).post(protect, admin, createProduct);
+router.get('/top', getTopProducts);
 router.route('/:id').get(getProductById).put(protect, admin, updateProduct).delete(protect, admin, deleteProduct);
 router.route('/:id/reviews').post(protect, createdProductReview);
 
--- frontend/src/slices/productsApiSlice.js
+++ frontend/src/slices/productsApiSlice.js
@@ -4,9 +4,10 @@ import { apiSlice } from "./apiSlice";
 export const productsApiSlice = apiSlice.injectEndpoints({
     endpoints: (builder) => ({
         getProducts: builder.query({
-            query: ({ pageNumber }) => ({
+            query: ({ keyword, pageNumber }) => ({
                 url: PRODUCTS_URL,
                 params: {
+                    keyword,
                     pageNumber,
                 }
             }),
@@ -55,6 +56,12 @@ export const productsApiSlice = apiSlice.injectEndpoints({
             }),
             invalidatesTags: ['Product'],
         }),
+        getTopProducts: builder.query({
+            query: () => ({
+                url: `${PRODUCTS_URL}/top`,
+            }),
+            keepUnusedDataFor: 5,
+        }),
     }),
 });
 
@@ -64,4 +71,5 @@ export const { useGetProductsQuery,
                useUpdateProductMutation, 
                useUploadProductImageMutation, 
                useDeleteProductMutation, 
-               useCreateReviewsMutation } = productsApiSlice;
+               useCreateReviewsMutation,
+               useGetTopProductsQuery } = productsApiSlice;
